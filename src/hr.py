42f3dc513112a202e6a9368ad78120595e505c95
Blase Gonzales <blasethegonzales79@gmail.com>
2024-11-07 19:46:44 +0800

    Feature1 Field

--- src/hr.py
+++ src/hr.py
@@ -1,2 +1,6 @@
 class VersionThree(Models.model):
     name = fields.Char("Name", store=True)
+
+class FeatureOne(Models.model):
+    name = fields.Char("Name", store=True)
+    email = fields.Char("Emailgit", store=True)
